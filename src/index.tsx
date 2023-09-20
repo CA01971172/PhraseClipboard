d7b3d1129f0420282fdde7f14e0c93d8f92c2119
†村神† <CA01971172@st.kawahara.ac.jp>
2023-09-20 11:38:21 +0900

    set environment

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./style/popup.scss"
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        hoge
+    </React.StrictMode>,
+    document.getElementById('root')
+);
