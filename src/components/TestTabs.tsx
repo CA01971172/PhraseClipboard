a1e955194ec81c962057450afaec777cc3ec0ccd
CA01971092 <107534446+CA01971092@users.noreply.github.com>
2023-10-19 10:15:38 +0900

    add deleteTab

--- src/components/TestTabs.tsx
+++ src/components/TestTabs.tsx
@@ -1,26 +1,28 @@
 import React, { useState, useContext } from 'react';
 import { DataContext } from "../providers/DataProvider" 
+import { rename } from 'fs';
+import { Menu } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function TestTabs (){
     const [text, setText] = useState<string>("");
-        const handleLiText = ( LiText:string ) => {
-        setText( LiText )
-    }
+    
     const {
         tabArray,
         addTab,
-        renameTab
+        renameTab,
+        deleteTab
     }= useContext(DataContext);
 
     return (
-       <div>
+        <div>
             <button onClick={() => addTab()}>+</button>
             <input
                 type="text"
                 value={text}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                     const newText: string = event.target.value;
-                    console.log(`変更されました:`, newText);
+                    console.log(`変更されました:`, );
                     setText(newText);
                 }}
             />
@@ -28,12 +30,15 @@ export default function TestTabs (){
                 renameTab(text, 0);
             }}>出力</button>
 
+            <button onClick={() => {
+                deleteTab(0)
+            }}>削除</button>
             <ul>
                 {Object.values(tabArray).map((tabData, index) => (
-                    <li key={index} onClick ={() => handleLiText(tabData.tabName)} >
+                    <li key={index} onClick ={() => renameTab(text, index)}
+                    { ...() => deleteTab(index)} >
                     {tabData.tabName}
                     </li>
-                    
                 ))}
             </ul>
        </div>
