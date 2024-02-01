ed317bc661dcd8144bbaaeb17b7e5118b010199a
CA01971092 <107534446+CA01971092@users.noreply.github.com>
2024-02-01 11:21:33 +0900

    hotfix DropdownMenu

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,108 +1,24 @@
 import * as React from 'react';
+import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import Fade from '@mui/material/Fade';
-import { useState, useContext } from 'react';
-import { DataContext } from "../providers/DataProvider" 
-import { formGroupClasses } from '@mui/material';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
-type Props = {
-  num: number;
-  open: boolean;
-  anchors: React.MutableRefObject<React.RefObject<HTMLDivElement>[]>;
-  handleTabChange: (event: React.SyntheticEvent, newValue: number) => void;
-  handleClose: () => Promise<void>;
-  focusedIndex: number;
-  setFocusedIndex: React.Dispatch<React.SetStateAction<number>>;
-}
-
-export default function DropdownMenu({
-  num,
-  open,
-  anchors,
-  handleTabChange,
-  handleClose,
-  focusedIndex,
-  setFocusedIndex
-}: Props) {
-  const [text, setText] = useState<string>("");
-  
-  const {
-    tabArray,
-    addTab,
-    swapTab
-  } = useContext(DataContext) || [];
-  
-  const {
-    renameTab,
-    deleteTab
-  } = useContext(DataContext);
-
+export default function DropdownMenu() {
   return (
-    <div>
-      <Menu
-        anchorEl={anchors.current[num]?.current}
-        open={open} // メニューの出現を管理
-        disableAutoFocusItem={false}
-        id="fade-menu"
-        MenuListProps={{
-          'aria-labelledby': 'fade-button',
-        }}
-        TransitionComponent={Fade}
-        onClose={async() => {
-          await handleClose();
-        }}
-      >
-
-        <div>
-        <input
-            type="text"
-            value={text}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              const newText: string = event.target.value;
-              console.log(`変更されました:`, );
-              setText(newText);
-          }}
-        />
-        </div>
-          <MenuItem onClick={() => deleteTab(focusedIndex)}>削除</MenuItem>
-          <MenuItem onClick={() => {
-            renameTab(text,focusedIndex)
-            handleClose();
-          }}>
-          名前を変更
-          </MenuItem>
-
-          <MenuItem
-            onClick={(event: React.SyntheticEvent) => {
-              swapTab(focusedIndex, -1); // 現在タブを左に移動する
-              handleTabChange(event,focusedIndex - 1)
-              handleClose();
-            }}
-            disabled={(0 > focusedIndex - 1) ? true : false}
-          >
-          左に移動
-          </MenuItem>
-
-          <MenuItem
-            onClick={(event: React.SyntheticEvent) => {
-            swapTab(focusedIndex, 1); // 現在タブを右に移動する
-            handleTabChange(event,focusedIndex + 1)
-            handleClose();
-            }}
-            disabled={(tabArray.length <= focusedIndex + 1) ? true : false}
-          >
-          右に移動
-          </MenuItem>
-
-          <MenuItem
-            onClick={(event: React.SyntheticEvent) => {
-              handleClose();
-            }}
-            >
-              Menuを閉じる
-          </MenuItem>
-        </Menu>
-      </div>
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <Button variant="contained" {...bindTrigger(popupState)}>
+            Dashboard
+          </Button>
+          <Menu {...bindMenu(popupState)}>
+            <MenuItem onClick={popupState.close}>Profile</MenuItem>
+            <MenuItem onClick={popupState.close}>My account</MenuItem>
+            <MenuItem onClick={popupState.close}>Logout</MenuItem>
+          </Menu>
+        </React.Fragment>
+      )}
+    </PopupState>
   );
 }
